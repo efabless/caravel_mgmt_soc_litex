897c7a42b2350433db36defbbe3679fe0b14ffb2
Mohamed Shalan <mshalan@aucegypt.edu>
2021-10-28 18:53:13 +0200

    added gpio, cleaned up the code, fixed OpenRAM integration, ....

--- OpenRAM.py
+++ OpenRAM.py
@@ -8,7 +8,6 @@ class OpenRAM(Module):
         self.bus = wishbone.Interface(width)
 
         # # #
-
         assert width in [32]
         assert size in [2*kB]
         depth_cascading = size//(1*kB)
@@ -21,30 +20,31 @@ class OpenRAM(Module):
                 datain   = Signal(32)
                 dataout  = Signal(32)
                 maskwren = Signal(4)
-                wren     = Signal()
+                wren_b   = Signal()
+                cs_b     = Signal()
                 self.comb += [
-                    
-                    datain.eq(self.bus.dat_w[16*w:16*(w+1)]),
-                    If(self.bus.adr[14:14+log2_int(depth_cascading)+1] == d,
-                        wren.eq(self.bus.we & self.bus.stb & self.bus.cyc),
-                        self.bus.dat_r[16*w:16*(w+1)].eq(dataout)
+                    datain.eq(self.bus.dat_w[0:32]),
+                    If(self.bus.adr[8:7+log2_int(depth_cascading)+1] == d,
+                        wren_b.eq(~(self.bus.we & self.bus.stb & self.bus.cyc)),
+                        self.bus.dat_r[0:32].eq(dataout),
+                        cs_b.eq(1)
                     ),
                     # maskwren is nibble based
-                    maskwren[0].eq(self.bus.sel[2*w + 0]),
-                    maskwren[1].eq(self.bus.sel[2*w + 0]),
-                    maskwren[2].eq(self.bus.sel[2*w + 1]),
-                    maskwren[3].eq(self.bus.sel[2*w + 1]),
+                    maskwren[0].eq(self.bus.sel[0]),
+                    maskwren[1].eq(self.bus.sel[1]),
+                    maskwren[2].eq(self.bus.sel[2]),
+                    maskwren[3].eq(self.bus.sel[3]),
                 ]
                 self.specials += Instance("sram_1rw1r_32_256_8_sky130",
                     i_clk0      = ClockSignal("sys"),
                     #i_STANDBY    = 0b0,
                     #i_SLEEP      = 0b0,
                     #i_POWEROFF   = 0b1,
-                    i_addr0   = self.bus.adr[:7],
+                    i_addr0   = self.bus.adr[:8],
                     i_din0    = datain,
                     i_wmask0  = maskwren,
-                    i_web0    = wren,
-                    i_csb0    = 0b1,
+                    i_web0    = wren_b,
+                    i_csb0    = cs_b,
                     o_dout0   = dataout
                 )
 
--- caravel.py
+++ caravel.py
@@ -10,27 +10,17 @@ import argparse
 from os import path
 
 from migen import *
-from migen.genlib.resetsync import AsyncResetSynchronizer
-
-from litex.soc.cores.ram import Up5kSPRAM
 from litex.soc.cores.spi_flash import SpiFlash
-#from litex.soc.cores.clock import iCE40PLL
 from litex.soc.integration.soc_core import SoCCore
 from litex.soc.integration.builder import Builder, builder_argdict, builder_args
-#from litex.build.lattice.programmer import IceStormProgrammer
 from litex.soc.integration.soc_core import soc_core_argdict, soc_core_args
 from litex.soc.integration.doc import AutoDoc
 from litex.soc.integration.soc import SoCRegion
 from litex.soc.integration.soc_core import *
-
 from litex.build.generic_platform import *
-#from litex.build.lattice import LatticePlatform
-
-#from litex_boards.platforms.icebreaker import Platform, break_off_pmod
-
 from litex.soc.cores.uart import UARTWishboneBridge
-#from rtl.leds import Leds
-
+from litex.soc.cores.gpio import GPIOTristate
+from litex.soc.cores.spi import SPIMaster, SPISlave
 import litex.soc.doc as lxsocdoc
 
 from CaravelChip import Platform
@@ -45,11 +35,7 @@ class MGMTSoC(SoCMini):
         "vexriscv_debug":   0xf00f0000,
     }
 
-    def __init__(self, sys_clk_freq=int(100e6),
-        with_gpio       = False, gpio_width=32,
-        with_spi_master = True, spi_master_data_width=1, spi_master_clk_freq=1e6,
-        **kwargs
-    ):
+    def __init__(self, sys_clk_freq=int(10e6), **kwargs ):
 
         platform = Platform("mgmt_soc")
 
@@ -60,36 +46,44 @@ class MGMTSoC(SoCMini):
         kwargs["csr_data_width"] = 32
         kwargs["cpu_reset_address"] = self.mem_map["spiflash"]
         kwargs["uart_name"]   = "crossover"
-
+        kwargs["with_timer"]   = True
         
+
         # CRG --------------------------------------------------------------------------------------
         self.submodules.crg = CRG(platform.request("sys_clk"), rst=platform.request("sys_rst"))
 
         #print(kwargs)
         SoCMini.__init__(self, platform, clk_freq=sys_clk_freq, **kwargs)
 
-        if with_spi_master:
-            platform.add_extension([
+        # Add a master SPI controller
+        platform.add_extension([
                 ("spi_master", 0,
                     Subsignal("clk",  Pins(1)),
                     Subsignal("cs_n", Pins(1)),
                     Subsignal("mosi", Pins(1)),
                     Subsignal("miso", Pins(1)),
                 )
-            ])
-            self.submodules.spi_master = SPIMaster(
+        ])
+        self.submodules.spi_master = SPIMaster(
                 pads         = platform.request("spi_master"),
-                data_width   = spi_master_data_width,
+                data_width   = 2,
                 sys_clk_freq = sys_clk_freq,
-                spi_clk_freq = spi_master_clk_freq,
-            )
+                spi_clk_freq = 1e5,
+        )
 
-        if with_gpio:
-            platform.add_extension([("gpio", 0, Pins(gpio_width))])
-            self.submodules.gpio = GPIOTristate(platform.request("gpio"))
-            self.add_csr("gpio")
-
+        # Add a GPIO Pin
+        platform.add_extension([("gpio", 0, Pins(1))])
+        self.submodules.gpio = GPIOTristate(platform.request("gpio"))
+        #self.add_csr("gpio")
         
+        # Add a master wb port
+        wb_bus = wishbone.Interface()
+        self.bus.add_master(master=wb_bus)
+        platform.add_extension(wb_bus.get_ios("wb"))
+        wb_pads = platform.request("wb")
+        self.comb += wb_bus.connect_to_pads(wb_pads, mode="master")
+        
+
         #Use OpenRAM
         spram_size = 2 * 1024
         self.submodules.spram = OpenRAM(size=spram_size)
@@ -111,59 +105,13 @@ class MGMTSoC(SoCMini):
         self.add_wb_master(self.uart_bridge.wishbone)
 
 
-
-
-# Build --------------------------------------------------------------------------------------------
-
-def soc_argdict(args):
-    ret = {}
-    for arg in [
-        "with_pwm",
-        "with_mmcm",
-        "with_uart",
-        "uart_fifo_depth",
-        "with_ctrl",
-        "with_timer",
-        "with_gpio",
-        "gpio_width",
-        "with_spi_master",
-        "spi_master_data_width",
-        "spi_master_clk_freq",
-        "csr_data_width",
-        "csr_address_width",
-        "csr_paging"]:
-        ret[arg] = getattr(args, arg)
-    return ret
-
 def main():
-    parser = argparse.ArgumentParser(description="LiteX standalone core generator")
-    builder_args(parser)
-
-    # Cores
-    #parser.add_argument("--with-pwm",              action="store_true",   help="Add PWM core")
-    #parser.add_argument("--with-mmcm",             action="store_true",   help="Add MMCM (Xilinx 7-series) core")
-    #parser.add_argument("--with-uart",             action="store_true",   help="Add UART core")
-    #parser.add_argument("--uart-fifo-depth",       default=16, type=int,  help="UART FIFO depth (default=16)")
-    #parser.add_argument("--with-ctrl",             action="store_true",   help="Add bus controller core")
-    parser.add_argument("--with-timer",            action="store_true",   help="Add timer core")
-    parser.add_argument("--with-spi-master",       action="store_true",   help="Add SPI master core")
-    parser.add_argument("--spi-master-data-width", default=8,   type=int, help="SPI master data width")
-    parser.add_argument("--spi-master-clk-freq",   default=8e6, type=int, help="SPI master output clock frequency")
-    parser.add_argument("--with-gpio",             action="store_true",   help="Add GPIO core")
-    parser.add_argument("--gpio-width",            default=32,  type=int, help="GPIO signals width")
-
-    # CSR settings
-    parser.add_argument("--csr-data-width",    default=8,     type=int, help="CSR bus data-width (8 or 32, default=8)")
-    parser.add_argument("--csr-address-width", default=14,    type=int, help="CSR bus address-width")
-    parser.add_argument("--csr-paging",        default=0x800, type=int, help="CSR bus paging")
-
-    args = parser.parse_args()
-
-    soc     = MGMTSoC(**soc_argdict(args))
-    builder = Builder(soc, **builder_argdict(args))
+    soc     = MGMTSoC()
+    builder = Builder(soc, compile_software=False)
     builder.build()
 
     lxsocdoc.generate_docs(soc, "build/documentation/", project_name="Caravel Management SoC", author="Efabless")
+
 
 if __name__ == "__main__":
     main()
