59891b0b5b53bfd664e2ba06398fb8be2a83e80b
Mohamed Shalan <mshalan@aucegypt.edu>
2021-11-03 14:27:37 +0200

    few TB fixes

--- sim/mgmt_soc_tb.v
+++ sim/mgmt_soc_tb.v
@@ -36,16 +36,16 @@ module mgmt_soc_tb;
     // PoR
     initial begin
         core_rst <= 1'b0;
-        #1000;
+        #500;
         core_rst <= 1'b1;
-		#10000;
+		#5000;
 		core_rst <= 1'b0;
 	end
 
     initial begin
 		$dumpfile("mgmt_soc_tb.vcd");
 		$dumpvars(0, mgmt_soc_tb);
-
+/*
 		// Repeat cycles of 1000 clock edges as needed to complete testbench
 		repeat (25) begin
 			repeat (1000) @(posedge core_clk);
@@ -53,13 +53,14 @@ module mgmt_soc_tb;
 		end
 		$display("%c[1;31m",27);
 		$display ("Monitor: Timeout, Test Failed");
-		$display("%c[0m",27);
+		$display("%c[0m",27);*/
+		#12_000;
 		$finish;
 	end
 
     top muv (
         .core_clk(core_clk),
-        .core_rst(),
+        .core_rst(core_rst),
         /*
         reg serial_rx,
         wire serial_tx,
@@ -71,12 +72,13 @@ module mgmt_soc_tb;
         .spiflash_cs_n(spiflash_cs_n),
         .spiflash_clk(spiflash_clk),
         .spiflash_miso(spiflash_miso),
-        .spiflash_mosi(spiflash_mosi)
+        .spiflash_mosi(spiflash_mosi),
+        .serial_dbg_rx(1'b0),
+        .serial_dbg_tx()
+
         /*
         .spiflash_wp(),
         .spiflash_hold(),
-        reg serial_dbg_rx,
-        wire serial_dbg_tx,
         output wire gpio_out_pad,
         reg gpio_in_pad,
         output wire gpio_outenb_pad,
