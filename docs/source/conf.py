4c7d9be6fd7a0d975736a5bf1b5aaf68661cfae9
jeffdi <jeffdi@efabless.com>
2022-04-09 13:31:53 -0700

    update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,8 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from ..generated.conf import *
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -33,9 +35,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Caravel Mgmt SoC Litex'
+# project = 'Caravel Mgmt SoC Litex'
 copyright = '2022, Efabless'
-author = 'Efabless'
+# author = 'Efabless'
 
 
 # -- General configuration ---------------------------------------------------
@@ -43,24 +45,24 @@ author = 'Efabless'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-  'sphinxcontrib.wavedrom',
-  'sphinx.ext.mathjax',
-  'sphinx.ext.todo'
-]
+# extensions = [
+#   'sphinxcontrib.wavedrom',
+#   'sphinx.ext.mathjax',
+#   'sphinx.ext.todo'
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [
-    'build',
-    'Thumbs.db',
-    # Files included in other rst files.
-    'introduction.rst',
-]
+# exclude_patterns = [
+#     'build',
+#     'Thumbs.db',
+#     # Files included in other rst files.
+#     'introduction.rst',
+# ]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -82,7 +84,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 todo_include_todos = False
 
